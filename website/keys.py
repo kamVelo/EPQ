a95004c5a6c4510f3acd53e3ac77e15af40e3cc7
Ali Kamel <alik87932@gmail.com>
2022-08-06 03:08:47 +0100

    removed local only files

--- website/keys.py
+++
@@ -1,9 +0,0 @@
-"""
-this file won't go on the github to protect passwords and data
-"""
-
-def checkPassword(attempt):
-    if attempt == "theySpeakEnglishInWhat?":
-        return True
-    else:
-        return False
